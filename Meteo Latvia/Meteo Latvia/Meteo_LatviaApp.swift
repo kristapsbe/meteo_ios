a70dbe28960d73134a841cb6f3c86a22e9f6c91c
Kristaps <kbebris+git@gmail.com>
2024-10-09 22:52:09 +0300

    init app

---
+++ Meteo Latvia/Meteo Latvia/Meteo_LatviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meteo_LatviaApp.swift
+//  Meteo Latvia
+//
+//  Created by Kristaps on 09/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Meteo_LatviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
